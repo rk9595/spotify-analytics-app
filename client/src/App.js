866ccd9c2b62157a7bcd49809ad6a9df4c48e35d
Rakesh Kariya <83279947+rk9595@users.noreply.github.com>
2022-11-21 09:22:16 +0000

    feat: authflow

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+         
+        >
+          Login
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
